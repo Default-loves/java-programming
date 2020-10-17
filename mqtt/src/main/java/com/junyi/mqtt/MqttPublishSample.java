1b37813a49186d9bc8c8c5f13e88b2c86ac69011
Default-loves <2111605130@mail2.gdut.edu.cn>
2020-10-17 17:57:29 +0800

    update

--- mqtt/src/main/java/com/junyi/mqtt/MqttPublishSample.java
+++ mqtt/src/main/java/com/junyi/mqtt/MqttPublishSample.java
@@ -16,26 +16,27 @@ public class MqttPublishSample {
 
     public static void main(String[] args) {
 
-        String topic        = "MQTT Examples";
-        String content      = "Message from MqttPublishSample";
+        String topic        = "1/apple";
+        String content      = "Message from 123";
         int qos             = 2;
-        String broker       = "tcp://test.mosquitto.org:1883";
+        String broker       = "tcp://localhost:1883";
         String clientId     = "JavaSample";
         MemoryPersistence persistence = new MemoryPersistence();
 
         try {
-            MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
+            MqttClient client = new MqttClient(broker, clientId, persistence);
             MqttConnectOptions connOpts = new MqttConnectOptions();
             connOpts.setCleanSession(true);
             System.out.println("Connecting to broker: "+broker);
-            sampleClient.connect(connOpts);
+            client.connect(connOpts);
             System.out.println("Connected");
+
             System.out.println("Publishing message: "+content);
             MqttMessage message = new MqttMessage(content.getBytes());
             message.setQos(qos);
-            sampleClient.publish(topic, message);
-            System.out.println("Message published");
-            sampleClient.disconnect();
+            client.publish(topic, message);
+
+            client.disconnect();
             System.out.println("Disconnected");
             System.exit(0);
         } catch(MqttException me) {
