dd98c7695f42ac5893202519758fd9ec228f1235
Default-loves <2111605130@mail2.gdut.edu.cn>
2020-10-31 18:32:38 +0800

    update

--- common/src/main/java/com/junyi/aaa.java
+++ common/src/main/java/com/junyi/aaa.java
@@ -4,6 +4,8 @@ package com.junyi;
 import com.junyi.entity.Book;
 import lombok.extern.slf4j.Slf4j;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 import java.util.stream.Stream;
 
@@ -18,14 +20,8 @@ import java.util.stream.Stream;
 @Slf4j
 public class aaa {
     public static void main(String[] args) {
-        byte[] array1 = new byte[4 * 1024 * 1024];
-        array1 = null;
-
-        byte[] array2 = new byte[2 * 1024 * 1024];
-        byte[] array3 = new byte[2 * 1024 * 1024];
-        byte[] array4 = new byte[2 * 1024 * 1024];
-        byte[] array5 = new byte[128 * 1024];
-
-        byte[] array6 = new byte[2 * 1024 * 1024];
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd'T'HHmmss'Z'");
+        log.info(LocalDateTime.now().format(formatter));
+        log.info(LocalDateTime.now().toString());
     }
 }
